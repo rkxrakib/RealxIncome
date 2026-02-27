0e36c8e4651a7f3f39a5ea67878ebfd5a7691c20
rkxrakib <rakibhassanfb2@gmail.com>
2026-02-27 11:35:41 +0600

    Create index.php

---
+++ active/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><link rel="stylesheet" href="style.css"></head>
+<body>
+    <div class="container" style="text-align: center;">
+        <h3>Activate Account</h3>
+        <p>Fee: 100 TK</p>
+        <button id="actBtn">Activate Now</button>
+    </div>
+    <script type="module" src="main.js"></script>
+</body>
+</html>
